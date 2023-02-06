561325918dcfd251365673a7ed280078a83a5320
Nikita Popkov <yongeryk@gmail.com>
2023-02-06 16:53:53 +0500

    Initial Commit

---
+++ LearnCppSynergy/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LearnCppSynergy
+//
+//  Created by Nikita Popkov on 06.02.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
