83d06b3767302cfdad3a8dc9ce476621f735139f
Nikita Popkov <yongeryk@gmail.com>
2023-02-10 10:31:17 +0500

    if statement task

--- LearnCppSynergy/main.cpp
+++ LearnCppSynergy/main.cpp
@@ -1,9 +1,23 @@
-#include<cstdio>
 #include<iostream>
 
 int main() {
-//    system("chcp 1251>nul"); // for windows
-    printf("Hello");
-//    system("pause>nul"); // for windows
+    system("chcp 1251>nul");
+    
+    int a,b;
+    printf("Введите первое число: ");
+    scanf("%i", &a);
+    
+    printf("Введите второе число: ");
+    scanf("%i", &b);
+    
+    if (a > b) {
+        printf("Больше\n");
+    } else if (a < b) {
+        printf("Меньше\n");
+    } else {
+        printf("Равны\n");
+    }
+    
+    system("pause>nul");
     return 0;
 }
